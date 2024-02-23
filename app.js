475a97961546c71879f3bd99812603feb6e25244
Rose <i.a.rose.bk@gmail.com>
2024-02-23 14:20:52 -0500

    first commit

--- app.js
+++ app.js
@@ -1,29 +1,25 @@
 // app.js
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
 
-const hatsController= require("./Controllers/hatsController");
-
-
+const hatsController = require("./Controllers/hatsController");
 
 // // Middleware
 app.use(cors());
 app.use(express.json());
 
 // ROUTES
-app.use("/hats",hatsController);
+app.use("/hats", hatsController);
 
 app.get("/", (req, res) => {
-    res.send("Hello")
+  res.send("Hello");
 });
 
-
 // 404 Route Handler
-app.get("*",(req, res, next) => {
-    res.status(404).send('Sorry, the page you are looking for does not exist.');
-  });
+app.get("*", (req, res, next) => {
+  res.status(404).send("Sorry, the page you are looking for does not exist.");
+});
 
-
-module.exports= app 
+module.exports = app;
